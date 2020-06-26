341937aa104f62e4249894b3016fac2a5b190dfe
John Ahn <john.ahn@onyxdesign.net>
2020-06-25 17:42:58 -0700

    updated returned forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,15 @@
 const request = require('request');
 
+
+
+// goal: add new data to forecast
+// 1. update the forecast string to include new data
+// 2. commit your changes
+// 3. push your changes to github and deploy to heroku
+// 4. test your work in the live application!
+
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=1df80f7afd161e5af08c78f1f17297fe&query=' + latitude + ',' + longitude + '&units=f';
 
@@ -9,7 +19,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            const forecast = 'It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.';
+            const forecast = 'It is currently ' + body.current.temperature + ' degrees out. It is ' + body.current.weather_descriptions[0].toLowerCase() + ' and feels like ' + body.current.feelslike + ' degrees.';
             callback(undefined, forecast);
         }
     });
